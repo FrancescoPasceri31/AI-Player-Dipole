d7ca5010c647639bd20bc3a75811b6ed438c423a
Domenico Montesanto <domenico.montesanto1@gmail.com>
2019-10-31 18:25:21 +0100

    Aggiustate tutte le tabelle hash e salvate correttamente su file (hashMap)

--- IntelligenzaArtificiale/src/testing/HashMapGenerator.java
+++ IntelligenzaArtificiale/src/testing/HashMapGenerator.java
@@ -1,6 +1,12 @@
 package testing;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
@@ -13,7 +19,7 @@ public class HashMapGenerator {
 //
 //		HashMap<Byte, String> posToCell = new HashMap();
 //		HashMap<String, Byte> cellToPos = new HashMap();
-
+//
 //		int x = 0;
 //		int y = 2;
 //
@@ -33,82 +39,112 @@ public class HashMapGenerator {
 //		for (Entry<Byte, String> e : posToCell.entrySet()) {
 //			cellToPos.put(e.getValue(), e.getKey());
 //		}
-
+//
 //		System.out.println(posToCell.toString());
 //		System.out.println(cellToPos.toString());
-
+//
 //		File f = new File("hashMap");
 //		ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream(f));
 //		f.createNewFile();
 //		o.writeObject(posToCell);
 //		o.writeObject(cellToPos);
+////		o.close();
+//
+//		Scacchiera s = new Scacchiera();
+//		MaskGenerator mg = new MaskGenerator(s);
+//		List<String> BLACK_MASK = mg.getBlackMask();
+//		List<String> Indietro_BLACK_MASK = mg.getIndietro_BLACK_MASK();
+//		List<String> WHITE_MASK = mg.getWhiteMask();
+//		List<String> Indietro_WHITE_MASK = mg.getIndietro_WHITE_MASK();
+//
+//		HashMap<Byte, Object[]> masksBlack = new HashMap();
+//		HashMap<Byte, Object[]> masksWhite = new HashMap();
+//
+//		// BLACK
+//		for (int i = 7; i >= 0; i--) {
+//			for (int j = 3; j >= 0; j--) {
+//				int k = (i * 4) + j;
+//				HashMap<Byte, Long[]> m = new HashMap();
+//				for (int z = 1; z <= 12; z++) {
+//					m.put((byte) z, new Long[] {
+//							Long.parseUnsignedLong(
+//									charToString(editMask(BLACK_MASK.get(31 - k).toCharArray(), z, 31 - k)), 2),
+//							Long.parseUnsignedLong(
+//									charToString(editMask(Indietro_BLACK_MASK.get(31 - k).toCharArray(), z, 31 - k)),
+//									2) });
+//				}
+//				masksBlack.put((byte) (k), new Object[] { 0, m }); // new Object { #pedine, hashMap<numPedine,Maschere>
+//																	// }
+//			}
+//		}
+//
+//		// WHITE
+//		for (int i = 7; i >= 0; i--) {
+//			for (int j = 3; j >= 0; j--) {
+//				int k = (i * 4) + j;
+//				HashMap<Byte, Long[]> n = new HashMap();
+//				for (int z = 1; z <= 12; z++) {
+//					n.put((byte) z, new Long[] {
+//							Long.parseUnsignedLong(
+//									charToString(editMask(WHITE_MASK.get(31 - k).toCharArray(), z, 31 - k)), 2),
+//							Long.parseUnsignedLong(
+//									charToString(editMask(Indietro_WHITE_MASK.get(31 - k).toCharArray(), z, 31 - k)),
+//									2) });
+//				}
+//				masksWhite.put((byte) (k), new Object[] { 0, n }); // new Object { #pedine, hashMap<numPedine,Maschere>
+//																	// }
+//			}
+//		}
+//
+//		System.out.println();
+//
+//		o.writeObject(masksBlack);
+//		o.writeObject(masksWhite);
 //		o.close();
-
-//		long t = System.currentTimeMillis();
-//		ObjectInputStream i = new ObjectInputStream(new FileInputStream("hashMap"));
-//		HashMap<Byte, String> posToCell = (HashMap<Byte, String>)i.readObject();
-//		HashMap<Byte, String> cellToPos = (HashMap<Byte, String>)i.readObject();
 //		
-//		i.close();
-//		System.out.println((System.currentTimeMillis()-t)/1000.0);
-//		System.out.println();
-//		System.out.println(posToCell.toString());
-//		System.out.println();
-//		System.out.println(cellToPos.toString());
-
-		Scacchiera s = new Scacchiera();
-		MaskGenerator mg = new MaskGenerator(s);
-		List<String> BLACK_MASK = mg.getBlackMask();
-		List<String> Indietro_BLACK_MASK = mg.getIndietro_BLACK_MASK();
-		List<String> WHITE_MASK = mg.getWhiteMask();
-		List<String> Indietro_WHITE_MASK = mg.getIndietro_WHITE_MASK();
-
-		HashMap<Byte, Object[]> masksBlack = new HashMap();
-
-		for (int i = 7; i >= 0; i--) {
-			for (int j = 3; j >= 0; j--) {
-				int x = (i * 4) + j;
-				HashMap<Byte, Long[]> m = new HashMap();
-				for (int z = 1; z <= 12; z++) {
-					m.put((byte) z, new Long[] {
-							Long.parseUnsignedLong(charToString(editMask(BLACK_MASK.get(x).toCharArray(), z, x)), 2),
-							Long.parseUnsignedLong(
-									charToString(editMask(Indietro_BLACK_MASK.get(x).toCharArray(), z, x)), 2) });
-				}
-				masksBlack.put((byte) (x), new Object[] { 0, m }); // new Object { #pedine, hashMap<numPedine,Maschere>
-																	// }
-			}
-		}
-		System.out.println();
 //		System.out.println(masksBlack);
-		System.out.print("{ ");
-		for (Entry<Byte, Object[]> e : masksBlack.entrySet()) {
-			System.out.print("'" + e.getKey() + "' : {\n");
-			Object[] o = e.getValue();
-			HashMap<Byte, Long[]> hTmp = (HashMap<Byte, Long[]>) o[1];
-			for (Entry<Byte, Long[]> e1 : hTmp.entrySet()) {
-				byte numPedine = e1.getKey();
-				Long[] maschere = e1.getValue();
-				System.out.print("\t\t'" + numPedine + "' : [ " + maschere[0] + " , " + maschere[1] + " ]\n");
-			}
-		}
-		System.out.println(" }");
-		System.out.println();
-//		System.out.println(masksBlack.get(31));
-//		System.out.println();
-		long m = Long.parseUnsignedLong(charToString(editMask(BLACK_MASK.get(9).toCharArray(), 1, 9)), 2);
-		long p = Long.parseUnsignedLong(charToString(editMask(Indietro_BLACK_MASK.get(9).toCharArray(), 1, 9)), 2);
-		long c = Long.parseUnsignedLong("00000001010000000000000001000000", 2); // centrato in 22
-		long e = Long.parseUnsignedLong("01001000100000000100000000000000", 2); // nemico
 
-		System.out.println("Nero");
-		Test.stampaScacchiera(Test.onesPosition(c));
+//      Test sulla tabella 
+
+//		System.out.println(charToString(editMask(WHITE_MASK.get(9).toCharArray(), 3, 9)));
+//		long l = ((Long[]) ((HashMap) masksWhite.get((byte) 22)[1]).get((byte) 3))[0];
+//		System.out.println();
+//		System.out.println(Long.toBinaryString(l));
+//		System.out.println();
+
+//      Test mosse
+
+//		long m = Long.parseUnsignedLong(charToString(editMask(BLACK_MASK.get(9).toCharArray(), 1, 9)), 2);
+//		long p = Long.parseUnsignedLong(charToString(editMask(Indietro_BLACK_MASK.get(9).toCharArray(), 1, 9)), 2);
+//		long c = Long.parseUnsignedLong("00000001010000000000000001000000", 2); // centrato in 22
+//		long e = Long.parseUnsignedLong("01001000100000000100000000000000", 2); // nemico
+//
+//		System.out.println("Nero");
+//		Test.stampaScacchiera(Test.onesPosition(c));
+//		System.out.println();
+//		System.out.println("Bianco");
+//		Test.stampaScacchiera(Test.onesPosition(e));
+//		System.out.println();
+//		System.out.println("Mosse");
+//		Test.stampaScacchiera(Test.zerosPosition(m & (p | (~e))));
+
+		ObjectInputStream i = new ObjectInputStream(new FileInputStream("hashMap"));
+		
+		long t = System.currentTimeMillis();
+		HashMap<Byte, String> posToCell = (HashMap<Byte, String>)i.readObject();
+		HashMap<Byte, String> cellToPos = (HashMap<Byte, String>)i.readObject();
+		HashMap<Byte, Object[]> masksBlack = (HashMap<Byte,Object[]>)i.readObject();
+		HashMap<Byte, Object[]> masksWhite = (HashMap<Byte,Object[]>)i.readObject();
+		i.close();
+		
+		System.out.println((System.currentTimeMillis()-t)/1000.0);
 		System.out.println();
-		System.out.println("Bianco");
-		Test.stampaScacchiera(Test.onesPosition(e));
+		System.out.println(posToCell.toString());
 		System.out.println();
-		System.out.println("Mosse");
-		Test.stampaScacchiera(Test.zerosPosition(m & (p | (~e))));
+		System.out.println(cellToPos.toString());
+		System.out.println();
+		printHash(masksBlack);
+		printHash(masksWhite);
 
 	}
 
@@ -132,6 +168,23 @@ public class HashMapGenerator {
 			ret += k;
 		}
 		return ret;
+	}
+
+	static void printHash(HashMap<Byte, Object[]> hp) {
+
+		System.out.print("{ ");
+		for (Entry<Byte, Object[]> e : hp.entrySet()) {
+			System.out.print("'" + e.getKey() + "' : {\n");
+			Object[] o = e.getValue();
+			HashMap<Byte, Long[]> hTmp = (HashMap<Byte, Long[]>) o[1];
+			for (Entry<Byte, Long[]> e1 : hTmp.entrySet()) {
+				byte numPedine = e1.getKey();
+				Long[] maschere = e1.getValue();
+				System.out.print("\t\t'" + numPedine + "' : [ " + maschere[0] + " , " + maschere[1] + " ]\n");
+			}
+		}
+		System.out.println(" }");
+
 	}
 
 }
--- IntelligenzaArtificiale/src/testing/MaskGenerator.java
+++ IntelligenzaArtificiale/src/testing/MaskGenerator.java
@@ -61,7 +61,7 @@ public class MaskGenerator {
 			for (int k = 0; k < scacchiera.getRowNumber(); k++) {
 				for (int l = 0; l < scacchiera.getColumnNumber(); l++) {
 					if ((scacchiera.getCell(k, l).colore).equals("B")) {
-						if (k > i||((k==i)&l==j)) { 	// riga avanti
+						if (k > i||((k==i)&&l==j)) { 	// riga avanti
 							mask += "1";
 						} else {	// riga dietro
 							int offset = Math.abs(k - i);
@@ -81,7 +81,7 @@ public class MaskGenerator {
 			for (int k = 0; k < scacchiera.getRowNumber(); k++) {
 				for (int l = 0; l < scacchiera.getColumnNumber(); l++) {
 					if ((scacchiera.getCell(k, l).colore).equals("B")) {
-						if (k < i||((k==i)&l==j)) {
+						if (k < i||((k==i)&&l==j)) {
 							mask += "1";
 						} else {
 							int offset = Math.abs(k - i);
